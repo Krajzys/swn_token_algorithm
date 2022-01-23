6e0d66190064438e9dbd0ace1b513fe848e1063f
Krajzys <kryzyyspl@gmail.com>
2022-01-23 13:56:22 +0100

    Finish project

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 from mpi4py import MPI
 import node as nd
 import time
+import random
+import argparse
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-count = comm.Get_size()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--log', type=str, help="Specifies file to which logs should be saved", required=False)
+    parser.add_argument('--no-acks', action="store_true", help="Specifies that no acks should be accepted", required=False)
+    parser.add_argument('-s', '--success-rate', type=float, help="Specifies the chance that the message won't be sent", required=False)
 
-if rank == 0:
-    node = nd.Node(rank, comm, count)
-elif rank == 1:
-    node = nd.Node(rank, comm, count, deny_from=[0])
-else:
-    node = nd.Node(rank, comm, count)
+    args = parser.parse_args()
 
-if node.rank == 0:
-    node._has_token = True
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    count = comm.Get_size()
 
-node.enter_section()
-time.sleep(5)
-node.leave_section()
+    log_file_name = ""
+    success_rate = 1
+    no_acks = False
+    if args.log:
+        log_file_name = args.log
+    if args.success_rate:
+        success_rate = args.success_rate
+    if args.no_acks:
+        no_acks = True
+
+    node = nd.Node(rank, comm, count, log_file=log_file_name, success_rate = success_rate, no_acks=no_acks)
+    if node.rank == 0:
+        node._has_token = True
+
+    while True:
+        if random.random() > 0.3:
+            print(f"{rank} wants to enter section", flush=True)
+            node.enter_section()
+            print(f"{rank} is inside the critical section", flush=True)
+            time.sleep(5)
+            node.leave_section()
+            print(f"{rank} leaves critical section", flush=True)
+            time.sleep(2)
+        else:
+            node.run_pass_token()
+            print(f"{rank} sleeps for 10s", flush=True)
+            time.sleep(10)
+
+if __name__ == '__main__':
+    main()
--- node.py
+++ node.py
@@ -1,11 +1,12 @@
 from sys import stderr
 import threading
 from time import sleep, time
-from message import MSG_ACK, MSG_UND, MSG_TOK, MSG_REC, MSG_RCK, Message
+from message import MSG_ACK, MSG_TOK, MSG_REC, MSG_RCK, Message
 from random import random
+import logging
 
 class Node:
-    def __init__(self, rank, comm, node_count, success_rate=1, no_acks=False, deny_from=[]):
+    def __init__(self, rank, comm, node_count, success_rate=1, no_acks=False, deny_from=[], log_file=""):
         self.rank = rank
         self.comm = comm
         self.success_rate = success_rate
@@ -15,124 +16,119 @@ class Node:
         self._got_rec_ack = None
         self._turn = 0
         self._sending_recovery = False
-        self.no_acks = False
+        self.no_acks = no_acks
         self.deny_from = deny_from
+        if log_file != "":
+            logging.basicConfig(filename=log_file, level=logging.INFO, format=f'{self.rank}:%(message)s')
+
         self.sem = threading.Semaphore()
         self._init_receiver()
 
     def send(self, to, message: Message, tag=0):
         chance = random()
         if (self.no_acks and message.msg_type == MSG_ACK):
-            print(f"{self.rank}) Send error (No ACKs)", flush=True)
+            logging.log(logging.INFO, f"Send error (No ACKs)")
         elif (self.success_rate >= chance):
-            print(f"{self.rank}) Send succesful {message}", flush=True)
+            logging.log(logging.INFO, f"Send succesful {message}")
             self.comm.send(message, dest=to, tag=tag)
         elif (self.success_rate < chance):
-            print(f"{self.rank}) Send error (Probability)", flush=True)
-
-    def sure_send(self, to, message, tag=0, timeout=1):
-        def _sure_send():
-            got_ack = False
-            while not got_ack:
-                self.send(to, message, tag)
-                sleep(timeout)
-                with self.sem:
-                    got_ack = self._got_ack
-            with self.sem:
-                self._got_ack = None
-        sure_sender_thread = threading.Thread(target=_sure_send)
-        sure_sender_thread.start()
-        return sure_sender_thread
+            logging.log(logging.INFO, f"Send error (Probability)")
 
     def _init_receiver(self):
         def __receiver():
             while True:
                 msg: Message = self.comm.recv()
                 if msg.src in self.deny_from:
-                    print(f"{self.rank}) Receive Error (Deny from: {self.deny_from}) {msg}")
+                    logging.log(logging.INFO, f"Receive Error (Deny from: {self.deny_from}), MSG:{msg}")
                     continue
-                if msg.msg_type == MSG_UND:
-                    print(f"{self.rank}) Received UND {msg}, sending ACK", flush=True)
-                    res = Message(self.rank, msg.src, MSG_ACK, "OK", msg.turn)
-                    self.send(msg.src, res)
                 elif msg.msg_type == MSG_ACK:
-                    print(f"{self.rank}) Received ACK {msg} setting got ACK", flush=True)
+                    logging.log(logging.INFO, f"Received ACK, setting got ACK, MSG:{msg}")
                     with self.sem:
                         self._got_ack = True
                 elif msg.msg_type == MSG_RCK:
-                    print(f"{self.rank}) Received RCK {msg} setting got REC ACK", flush=True)
-                    if self._sending_recovery:
-                        self._got_rec_ack = True
+                    logging.log(logging.INFO, f"Received RCK, setting got REC ACK, MSG:{msg}")
+                    with self.sem:
+                        if self._sending_recovery:
+                            self._got_rec_ack = True
                 elif msg.msg_type == MSG_TOK:
-                    print(f"{self.rank}) Received TOK {msg}, sending ACK", flush=True)
-                    if msg.turn == self._turn:
-                        self._has_token = True
-                        self._got_ack = True
+                    logging.log(logging.INFO, f"Received TOK, sending ACK, MSG:{msg}")
+                    with self.sem:
+                        if msg.turn == self._turn:
+                            self._has_token = True
+                            self._got_ack = True
 
                     res = Message(self.rank, msg.src, MSG_ACK, "OK", msg.turn)
                     self.send(msg.src, res)
                 elif msg.msg_type == MSG_REC:
-                    print(f"{self.rank}) Received REC {msg}, sending RCK", flush=True)
+                    logging.log(logging.INFO, f"Received REC, sending RCK, MSG:{msg}")
                     res = Message(self.rank, msg.src, MSG_RCK, "RECOK", msg.turn)
                     self.send(msg.src, res)
+                    # If token was for us and for current turn then create token
                     if msg.dest == self.rank:
                         if msg.turn == self._turn:
                             self._has_token = True
                         else:
                             pass
-                    else:
+                    else: # If token was not for us then pass it through
                         if self._has_token:
                             pass
-                        if not self._sending_recovery:
-                            self._sending_recovery = True
-                            next_receiver = -1
-                            if self.rank < msg.src: # send to the left or send by the last one to first one
-                                if self.rank == 0:
-                                    next_receiver = self.node_count - 1 if ((self.rank - 1) < 0) else (self.rank - 1)
+                        sending_recovery = False
+                        with self.sem:
+                            sending_recovery = self._sending_recovery
+                        if not sending_recovery: # If we are not passing token already
+                            with self.sem:
+                                self._sending_recovery = True
+                            send_left = False
+
+                            if msg.src > self.rank:
+                                if msg.src == self.node_count - 1 and self.rank == 0:
+                                    send_left = False
                                 else:
-                                    next_receiver = (self.rank + 1) % self.node_count
-                            else: # send to to right or by the first one to the last one
-                                if self.rank == self.node_count-1:
-                                    next_receiver = (self.rank + 1) % self.node_count
+                                    send_left = True
+                            else:
+                                if msg.src == 0 and self.rank == self.node_count - 1:
+                                    send_left = True
                                 else:
-                                    next_receiver = self.node_count - 1 if ((self.rank - 1) < 0) else (self.rank - 1)
-                            pass_recovery_thread = threading.Thread(target = self.pass_recovery_token, args=[next_receiver, msg.dest, msg.turn])
+                                    send_left = False
+                            
+                            pass_recovery_thread = threading.Thread(target = self.pass_recovery_token, args=[msg.dest, msg.turn, send_left])
                             pass_recovery_thread.start()
                 else:
-                    print(f"{self.rank}) Received unknown message_type {msg}", flush=True)
+                    logging.log(logging.INFO, f"Received unknown message_type, MSG:{msg}")
         receiver_thread = threading.Thread(target = __receiver)
         receiver_thread.start()
 
     def enter_section(self):
-        print(f"{self.rank}) Waiting for critical section...")
-        while(not self._has_token):
+        logging.log(logging.INFO, f"Waiting for critical section...")
+        has_token = False
+        with self.sem:
+            has_token = self._has_token
+        while not has_token:
             sleep(1)
-        print(f"{self.rank}) Entering critical section")
+            with self.sem:
+                has_token = self._has_token
+        logging.log(logging.INFO, f"Entering critical section")
 
     def leave_section(self):
+        self.pass_token()
+        logging.log(logging.INFO, f"Leaving critical section")
+
+    def run_pass_token(self):
         pass_token_thread = threading.Thread(target = self.pass_token)
         pass_token_thread.start()
-        print(f"{self.rank}) Leaving critical section")
-
-    def pass_recovery_token(self, receiver_no, msg_dest, msg_turn):
-        while not self._got_rec_ack:
-            self._sending_recovery = True
-            if receiver_no == (self.rank + 1) % self.node_count:
-                receiver_no = self.node_count - 1 if ((self.rank - 1) < 0) else (self.rank - 1)
-            else:
-                receiver_no = (self.rank + 1) % self.node_count
-
-            msg = Message(self.rank, msg_dest, MSG_REC, "RECOVERY", msg_turn)
-            self.send(receiver_no, msg)
-            print(f"{self.rank}) No ACK after timeout, sending RECOVERY {msg} to {receiver_no}")
-            sleep(2)
-        self._sending_recovery = False
-        self._got_rec_ack = None
 
     def pass_token(self):
-        if self._has_token:
-            self._has_token = False
-            msg = Message(self.rank, (self.rank + 1) % self.node_count, MSG_TOK, "TOKEN", self._turn)
+        has_token = False
+        with self.sem:
+            has_token = self._has_token
+        if has_token:
+            with self.sem:
+                self._has_token = False
+            turn = self._turn
+            if self.rank == self.node_count - 1:
+                turn = ((self._turn + 1) % 2)
+
+            msg = Message(self.rank, (self.rank + 1) % self.node_count, MSG_TOK, "TOKEN", turn)
             receiver_no = (self.rank + 1) % self.node_count
             self.send(receiver_no, msg)
             sleep(2)
@@ -141,12 +137,35 @@ class Node:
             with self.sem:
                 got_ack = self._got_ack
             if not got_ack:
-                pass_recovery_thread = threading.Thread(target = self.pass_recovery_token, args=[receiver_no, receiver_no, self._turn])
+                pass_recovery_thread = threading.Thread(target = self.pass_recovery_token, args=[receiver_no, turn, True])
                 pass_recovery_thread.start()
             else:
                 with self.sem:
-                    self._got_ack = False
+                    self._got_ack = None
 
             self._turn = ((self._turn + 1) % 2)
         else:
-            print(f"{self._rank}) Doesn't have the token! (so he can't pass it)", file=stderr)
+            logging.log(logging.INFO, f"Doesn't have the token! (so he can't pass it)")
+
+    def pass_recovery_token(self, msg_dest, msg_turn, send_left=True):
+        got_rec_ack = False
+        with self.sem:
+            got_rec_ack = self._got_rec_ack
+        while not got_rec_ack:
+            self._sending_recovery = True
+            receiver_no = -1
+            if send_left:
+                receiver_no = (self.node_count - 1) if (self.rank == 0) else (self.rank - 1)
+            else:
+                receiver_no = (self.rank + 1) % self.node_count
+
+            msg = Message(self.rank, msg_dest, MSG_REC, "RECOVERY", msg_turn)
+            self.send(receiver_no, msg)
+            logging.log(logging.INFO, f"Sending RECOVERY to {receiver_no}, MSG:{msg}")
+            send_left = not send_left
+            sleep(2)
+            with self.sem:
+                got_rec_ack = self._got_rec_ack
+        with self.sem:
+            self._sending_recovery = False
+            self._got_rec_ack = None
